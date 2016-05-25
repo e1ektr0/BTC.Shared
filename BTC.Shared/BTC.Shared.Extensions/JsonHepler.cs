7f510b011580974a95d304d7ad206149ca414ea4
Feofilov Maxim <feofilov@live.ru>
2016-05-25 23:33:20 +0300

    Add json parse extension

---
+++ BTC.Shared/BTC.Shared.Extensions/JsonHepler.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BTC.Shared.Extensions
+{
+    public static class JsonHepler
+    {
+        public static T Json<T>(this string json) where T : class, new()
+        {
+            var jobject = JObject.Parse(json);
+            return JsonConvert.DeserializeObject<T>(jobject.ToString());
+        }
+    }
+}
